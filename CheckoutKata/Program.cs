040489f64dbe9da39f0d43122abbca5e7fda45c5
N. Fowler <nrfowler@gmail.com>
2019-01-22 18:09:07 -0600

    Added GetTotalTest

---
+++ CheckoutKata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Grocery
+    {
+        private float total =0;
+         
+        public float getTotal()
+        {
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CheckoutKataTests/GroceryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests
+{
+    [TestClass()]
+    public class GroceryTests
+    {
+        [TestMethod()]
+        public void GetTotalTest()
+        {
+            Grocery grocery = new Grocery();
+            Assert.AreEqual(0, grocery.getTotal());
+        }
+    }
+}
